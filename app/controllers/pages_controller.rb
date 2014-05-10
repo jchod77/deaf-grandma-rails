83dbe578787ff104a54df835cf91ada8ffd36ac3
Jason Chodera <jason.chodera@outlook.com>
2014-05-10 18:02:43 -0500

    added remaining deaf grandma functionality

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   def index
+    @grandma = params[:grandma]
+      # look in app/views/index.html.erb
+  end
+
+  def grandma
+    params[:user_input] =~ /[a-z]/ ? @grandma = "HUH!? SPEAK UP, SONNY?" : @grandma = "NO, NOT SINCE 1983!"
+    # render '/'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 LearningRails::Application.routes.draw do
   get '/' => 'pages#index'
+  post '/grandma' => 'pages#grandma'
 end
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
